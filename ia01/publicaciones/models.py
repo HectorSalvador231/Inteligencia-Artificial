8ae5205cbb27591063c76e7bdcbac8dfa0ca772e
HectorSalvador231 <azulcrus-@hotmail.com>
2023-10-08 15:43:55 -0600

    Tarea

---
+++ ia01/publicaciones/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Posts(models.Model):
+    title =  models.CharField(verbose_name='Titulo de la publicacion', max_length=150)
+    content = models.TextField(verbose_name='Agrega el contenido de la publicacion')
+    status = models.SmallIntegerField(verbose_name='Indica el estado de la publicacion')
+    publishedDate = models.DateTimeField('Especificar fecha de publicacion')
+
+
+    def __str__(self):
+        return self.title
+    
+
+    class Meta:
+        verbose_name = 'Publicacion'
+        verbose_name_plural = 'Publicaciones'
+    
+
